deaa62793e1566e896d1fa1e2c3570a9c4539f43
Jacob Garcia <yockub@gmail.com>
2026-05-22 14:24:31 -0400

    added metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,14 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+export async function generateMetadata() {
+  const posts = await getPosts();
+  return {
+    title: `Browse our ${posts.length} posts`,
+    description: 'Browse our latest posts!'
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
